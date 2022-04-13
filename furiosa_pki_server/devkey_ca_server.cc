40d2146d4a6cd58c3d51cbc2828edec02ba18eda
Hyuck Han <hyuck.han@furiosa.ai>
2022-04-13 20:56:30 +0900

    update server code

--- furiosa_pki_server/devkey_ca_server.cc
+++ furiosa_pki_server/devkey_ca_server.cc
@@ -6,22 +6,13 @@
 #include <string>
 
 #include <grpc/grpc.h>
-#include <grpcpp/security/server_credentials.h>
-#include <grpcpp/server.h>
-#include <grpcpp/server_builder.h>
 #include <grpcpp/server_context.h>
 
 #include "furiosa_ca_devkey.grpc.pb.h"
-#include "furiosa_ca_devkey.pb.h"
 #include "furiosa_pki_ca_devkey.h"
 #include "devkey_ca_server.h"
 
-using grpc::Server;
-using grpc::ServerBuilder;
 using grpc::ServerContext;
-using grpc::ServerReader;
-using grpc::ServerReaderWriter;
-using grpc::ServerWriter;
 using grpc::Status;
 using devkeyca::DevKeyCA;
 using devkeyca::SignDevKeyRequest;
@@ -36,4 +27,3 @@ Status DevKeyCAImpl::SignDevKey(ServerContext* context, const SignDevKeyRequest*
     response->set_ret(RetCode::OK);
     return Status::OK;
 }
-
